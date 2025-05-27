d0e65626d407e8ed05ee60ac800ec9db6038caab
anatolio-deb <nikiforova693@gmail.com>
2025-05-27 06:38:35 +0000

    fix buttons

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -2,18 +2,13 @@
   <v-container>
     <v-img class="mb-4" height="150" src="@/assets/logo.png" />
     <Header text1="Welcome to" text2="PicoVPN"></Header>
-    <v-row>
-      <v-col></v-col>
-      <v-col cols="1" md="1" xs="1">
-        <v-btn rounded="xl" size="large" color="indigo-lighten-4" variant="tonal" @click="toTry">
-          Try
-        </v-btn>
-      </v-col>
-      <v-col cols="1" md="6" xs="6">
-        <v-btn rounded="xl" size="large" color="cyan-lighten-4" variant="tonal" @click="toBuy">
-          Buy
-        </v-btn>
-      </v-col>
+    <v-row justify="center">
+      <v-btn class="mx-2" rounded="xl" size="large" color="indigo-lighten-4" variant="tonal" @click="toTry">
+        Try
+      </v-btn>
+      <v-btn class="mx-2" rounded=" xl" size="large" color="cyan-lighten-4" variant="tonal" @click="toBuy">
+        Buy
+      </v-btn>
     </v-row>
   </v-container>
 </template>
