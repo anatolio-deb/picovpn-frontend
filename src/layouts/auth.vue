875097ac80785ac7f4910683681330529184a463
anatolio-deb <nikiforova693@gmail.com>
2025-05-27 06:45:48 +0000

    disable email

--- src/layouts/auth.vue
+++ src/layouts/auth.vue
@@ -5,8 +5,8 @@
         </Header>
         <v-card class="mx-auto px-6 py-8" max-width="344">
             <v-form v-model="form" @submit.prevent="onSubmit">
-                <v-text-field v-model="email" :readonly="loading" :rules="[required]" class="mb-2" label="Email"
-                    clearable></v-text-field>
+                <!-- <v-text-field v-model="email" :readonly="loading" :rules="[required]" class="mb-2" label="Email"
+                    clearable></v-text-field> -->
 
                 <v-text-field type="password" v-model="password" :readonly="loading" :rules="[required]"
                     label="Password" placeholder="Enter your password" clearable></v-text-field>
