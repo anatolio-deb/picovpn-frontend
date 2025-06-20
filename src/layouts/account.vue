6957a0d32434b0258d9c50be5ff9c970f707fcb7
anatolio-deb <nikiforova693@gmail.com>
2025-06-20 13:24:38 +0000

    fix: update account layout to display user avatar and username correctly

--- src/layouts/account.vue
+++ src/layouts/account.vue
@@ -1,11 +1,10 @@
 <template>
     <v-app-bar :elevation="2">
         <template v-slot:prepend>
-            <v-app-bar-nav-icon>
-            </v-app-bar-nav-icon>
+            <v-icon-btn :icon="avatar"></v-icon-btn>
         </template>
 
-        <v-app-bar-title></v-app-bar-title>
+        <v-app-bar-title>{{ username }}</v-app-bar-title>
     </v-app-bar>
 </template>
 
@@ -16,11 +15,8 @@ const avatar = ref("");
 const username = ref("");
 
 onMounted(() => {
-    // avatar.value = initData.user.photo_url;
-    // username.value = initData.user.username;
-    console.log(parsedInitData.user.photo_url)
-    console.log(parsedInitData.user.username)
-
+    avatar.value = parsedInitData.user.photo_url;
+    username.value = parsedInitData.user.username;
 })
 
 
