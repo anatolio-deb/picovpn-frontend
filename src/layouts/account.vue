89b0fe9eb33a882485a5ed4f775f46e69107f9f8
anatolio-deb <nikiforova693@gmail.com>
2025-06-20 08:37:26 +0000

    avatar and username in account page

--- src/layouts/account.vue
+++ src/layouts/account.vue
@@ -1,10 +1,24 @@
 <template>
-    <p>Account</p>
-    {{ initData }}
+    <v-app-bar :elevation="2" rounded>
+        <v-card :append-avatar="avatar" class="mx-auto"
+            prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+            subtitle="prepend-avatar and append-avatar" title="Avatars">
+            <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+        </v-card>
+
+        <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-const initData = localStorage.getItem("initData")
+const initData: any = localStorage.getItem("initData");
+const avatar = ref("");
+const username = ref("");
+
+onMounted(() => {
+    avatar.value = initData.user.photo_url;
+    username.value = initData.user.username;
+})
 
 
 </script>
