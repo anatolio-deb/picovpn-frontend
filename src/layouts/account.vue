fec42cf4fde3d0b734f9160d3aa5886cdc989b8c
anatolio-deb <nikiforova693@gmail.com>
2025-07-04 09:19:35 +0000

    refactor

--- src/layouts/account.vue
+++ src/layouts/account.vue
@@ -19,17 +19,20 @@
                 </v-card>
             </v-row>
             <v-row>
-                <v-card class="mx-auto" href="https://apps.apple.com/us/app/cisco-secure-client/id1135064690"
-                    max-width="344" prepend-icon="mdi-apple" rel="noopener" subtitle="Download available"
-                    target="_blank" title="App Store" color="black">
-                </v-card>
-            </v-row>
-            <v-row>
-                <v-card class="mx-auto"
-                    href="https://play.google.com/store/apps/details?id=com.cisco.anyconnect.vpn.android.avf&hl=en"
-                    max-width="344" prepend-icon="mdi-google-play" rel="noopener" subtitle="Download available"
-                    target="_blank" title="Google Play" color="black">
-                </v-card>
+                <v-col>
+                    <v-card class="mx-auto" href="https://apps.apple.com/us/app/cisco-secure-client/id1135064690"
+                        prepend-icon="mdi-apple" rel="noopener" subtitle="Available for download" target="_blank"
+                        title="App Store" color="black">
+                    </v-card>
+                </v-col>
+                <v-col>
+
+                    <v-card class="mx-auto"
+                        href="https://play.google.com/store/apps/details?id=com.cisco.anyconnect.vpn.android.avf&hl=en"
+                        prepend-icon="mdi-google-play" rel="noopener" subtitle="Available for download" target="_blank"
+                        title="Google Play" color="black">
+                    </v-card>
+                </v-col>
             </v-row>
         </v-container>
     </v-main>
