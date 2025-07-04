461d267d8f33f2c8986a0c266d97b9af6242af97
anatolio-deb <nikiforova693@gmail.com>
2025-07-04 08:00:22 +0000

    refactor

--- src/layouts/account.vue
+++ src/layouts/account.vue
@@ -25,15 +25,15 @@
             </v-row>
             <v-row>
                 <v-card class="mx-auto" href="https://apps.apple.com/us/app/cisco-secure-client/id1135064690"
-                    max-width="344" max-height="244" prepend-icon="mdi-apple" rel="noopener"
-                    subtitle="Download available" target="_blank" title="App Store">
+                    max-width="344" prepend-icon="mdi-apple" rel="noopener" subtitle="Download available"
+                    target="_blank" title="App Store" color="black">
                 </v-card>
             </v-row>
             <v-row>
                 <v-card class="mx-auto"
                     href="https://play.google.com/store/apps/details?id=com.cisco.anyconnect.vpn.android.avf&hl=en"
-                    max-width="344" max-height="244" prepend-icon="mdi-google-play" rel="noopener"
-                    subtitle="Download available" target="_blank" title="Google Play">
+                    max-width="344" prepend-icon="mdi-google-play" rel="noopener" subtitle="Download available"
+                    target="_blank" title="Google Play" color="black">
                 </v-card>
             </v-row>
         </v-container>
